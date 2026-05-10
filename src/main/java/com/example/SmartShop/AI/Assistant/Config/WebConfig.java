5fb6c832b601565f6b82df211227bfe77c83aede
Ibℝⱥђim♛ Åmjãd <ibrahimamjad126@gmail.com>
2026-05-10 16:41:16 +0500

    Add Dockerfile and update WebConfig

--- src/main/java/com/example/SmartShop/AI/Assistant/Config/WebConfig.java
+++ src/main/java/com/example/SmartShop/AI/Assistant/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all API endpoints
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173")  // Both React dev servers
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://friendly-duckanoo-470a23.netlify.app")  // Both React dev servers
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")  // Allow all HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow cookies/credentials
